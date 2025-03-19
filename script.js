07da612a291e5fce9d72c657b46085cbac3fe673
rustamova148 <79011363+rustamova148@users.noreply.github.com>
2025-03-19 11:13:32 +0400

    update 2

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //* Arrayda tekrarlanan elementleri ayri array-e cixarmaq
+
 // let arr = [1,2,3,4,2,5,1];
 // let new_arr=[];
 // arr.forEach((el,index) =>{
@@ -7,7 +8,9 @@
 //     }
 // });
 // console.log(new_arr);
+
 //* Sozlerin palindrom oldugunu yoxlama funksiyasi
+
 // let word = 'nezrin';
 // let reversed = word.split('').reverse().join('');
 // console.log(reversed);
@@ -17,19 +20,25 @@
 // }
 // console.log(isPalindrome('nezrin'));
 // console.log(isPalindrome('ana'));
+
 //** 2 arrayda ortaq elementleri array kimi output-a cixarmaq
+
 // function commonElements(arr1,arr2){
 //    return arr1.filter(num => arr2.includes(num));
 // }
 // console.log(commonElements([1,2,3,4],[3,4,5,6]));
+
 //** 2 arrayi birlesdirib sonra elementlerini toplamaq
+
 // function sum(arr1,arr2){
 //     let combined = [...arr1,...arr2];
 //     let summed = combined.reduce((a, b) => a + b);
 //     return summed;
 // }
 // console.log(sum([1,2,3,4],[3,4,5,6]));
+
 //** Faktorial tapmaq
+
 // function findFactorial(n){
 //   let result = 1;
 //   for(let i = 1; i<=n; i++){
@@ -38,8 +47,10 @@
 //   return result;
 // }
 // console.log(findFactorial(5));
+
 //* funksiya str parametri olaraq cumle qebul edir ve biz o cumlede length deyeri en uzun olan sozu 
 //* ve onun length deyerini return etmeliyik.
+
 // function findLongestWordLength(str) {
 //     let word = str.split(' ');
 //     let longWordLength = 0;
@@ -53,6 +64,7 @@
 //     return longWordLength + ', ' + longWord;
 // }
 // console.log(findLongestWordLength("menim adim nezrindir"));
+
 //* kontrol etmeliyik ki eger soz samit ile baslayirsa ve ya samit ile davam edirse o samitleri 
 //* kesib hemin sozun sonuna birlesdiririk ve "ay" sozunu elave edirik eger sait ile baslayirsa 
 //* sadece sozun sonuna "way" artirir return edirik
@@ -72,6 +84,7 @@
 // console.log(convertWordBasedOnVowel("california"));
 
 //* 2ci parametrde verilen sozu cumle icerisinde tapib 3cu parametrde verilen soz ile evez etmeli
+
 // function myReplace(str, before, after){
 //     if(before.charAt(0) === before.charAt(0).toUpperCase()){
 //     let ifUpper = after.charAt(0).toUpperCase() + after.slice(1).toLowerCase();
@@ -83,7 +96,9 @@
 
 // console.log(myReplace("Let us go to the store", "store", "mall"));
 // console.log(myReplace("His name is Tom", "Tom", "john"));
+
 //* G herfidise yanina C gelecek, C herfidide yanina G gelecek , A herfidise yanina T , T herfidise yanina A gelecek.
+
 // function pairElement(str){
 //     let after = str.split("");
 //     let mapped = after.map(el => [el] );
